b3517700e8ea2d0a8f23cd360a18cfc6ee4f9e76
Benjamin Stemper <ben.m.stemper@gmail.com>
2017-11-17 19:41:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,19 +12,19 @@ print("Tensorflow version " + tf.__version__)
 
 
 # Configuration.
-train_filename = 'deep_impliedvol/labeled_data_toy/train_uniform.csv'
-validation_filename = 'deep_impliedvol/labeled_data_toy/validation_uniform.csv'
-test_filename = 'deep_impliedvol/labeled_data_toy/test_uniform.csv'
-feature_cols = [0]
-label_cols = [1]
+train_filename = 'deep_impliedvol/labeled_data/train_uniform.csv'
+validation_filename = 'deep_impliedvol/labeled_data/validation_uniform.csv'
+test_filename = 'deep_impliedvol/labeled_data/test_uniform.csv'
+feature_cols = [0, 1, 2]
+label_cols = [3]
 random_seed = 42
 
 def main_single():
 
-	nb_epochs = 1000
-	mini_batch_size = 100
-	nn_layer_sizes = [64, 32]
-	lr = 0.00005
+	nb_epochs = 100000
+	mini_batch_size = 2
+	nn_layer_sizes = [32, 16]
+	lr = 0.005
 	log_name = 'log_test.txt'
 	
 	# Read training and validation data named tuples into memory.
@@ -35,15 +35,15 @@ def main_single():
 											 label_cols)
 	
 	train(train_set, validation_set, nn_layer_sizes, lr, random_seed, nb_epochs, 
-	  mini_batch_size, log_name, print_train=True)
+	  mini_batch_size, log_name, print_log=True)
 
 def main_random_search():
 
 	nb_epochs = 10
 	mini_batch_size = 100
-	max_exp = 10
+	max_exp = 9
 	nb_learning_rates = 10
-	log_name = 'log.txt'
+	log_name = 'hyp_sweep_log.txt'
 
 	# Read training and validation data named tuples into memory.
 	train_set = import_labeled_csv_data(train_filename, feature_cols, 
@@ -52,17 +52,18 @@ def main_random_search():
 	validation_set = import_labeled_csv_data(validation_filename, feature_cols, 
 											 label_cols)
 
-	nn_combinations = [[2**i, 2**j] for i in range(1, max_exp +1) for j in range(1, i+1)]
+	nn_combinations = [[2**i, 2**j, 2**k] for i in range(2, max_exp +1) 
+						for j in range(2, i+1) for k in range(2, j+1)]
 
 	for nn_layer_sizes in nn_combinations:
 
 		for lr in 10**np.random.uniform(-6, 1, nb_learning_rates):
 
 			train(train_set, validation_set, nn_layer_sizes, lr, random_seed, nb_epochs, 
-	  			  mini_batch_size, log_name, print_train=True)
+	  			  mini_batch_size, log_name, print_log=True)
 
 
 if __name__ == '__main__':
 	
-	main_single()
+	main_random_search()
 
