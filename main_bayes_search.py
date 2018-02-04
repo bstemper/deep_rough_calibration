66e860b305654b81d998fe58a0b2528bb4fc7a5f
Benjamin Stemper <ben.m.stemper@gmail.com>
2018-02-04 19:15:28 +0100

    Updated bayes script to incorporate recent changes.

--- main_bayes_search.py
+++ main_bayes_search.py
@@ -15,7 +15,7 @@ from neural_network import *
 from train import *
 
 # Log file name
-filename = 'heston_3layer_1'
+filename = 'iv_3layer_2'
 
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 sys.stdout = open(filename + '.txt', 'w', 1)
@@ -33,7 +33,7 @@ def train_bayes(params):
 
     Arguments:
     ----------
-        params: list, shape=[nb_layers + 3,]
+        params: list, shape=[nb_layers + 2,]
         List of search space dimensions. Entries have to be tuples 
         (lower_bound, upper_bound) for Reals or Integers.
 
@@ -49,83 +49,79 @@ def train_bayes(params):
     layer_sizes = (2**np.array(params[:3])).tolist()
     learning_rate = 10**params[3]
     mini_batch_size = 2**params[4]
-    pkeep = params[5]
+    pkeep = 1
     hyper_params = [layer_sizes, learning_rate, mini_batch_size, pkeep]
     hyper_param_str = make_hyper_param_str(hyper_params)
 
     # Call train function
     tic = time.time()
-    log_df, validation_results = train(train_tuple, validation_tuple, hyper_params, 
-                                       nb_epochs, random_seed, verbose=True)
-    
-    elapsed_time = time.time()-tic
 
-    # Setting the objective function
-    valid_loss, valid_err2pc, valid_err1pc = validation_results
-    objective = valid_err2pc
+    log_df, best_error = train(train_tuple, validation_tuple, hyper_params, 
+                               nb_epochs, random_seed, verbose=True)
     
+    elapsed_time = time.time() - tic
+
     # Writing Pandas log file to csv file on disk.
     log_df.to_csv(hyper_param_str + '/log_file.csv')
     
-    return objective, elapsed_time
+    return best_error, elapsed_time
 
-## IMPLIED VOLATILITY
-## ===========================================================================
-
-# # Data directories & data extraction params
-# train_filename = 'deep_impliedvol/labeled_data/train_uniform.csv'
-# validation_filename = 'deep_impliedvol/labeled_data/validation_uniform.csv'
-# test_filename = 'deep_impliedvol/labeled_data/test_uniform.csv'
-# feature_cols = [0, 1, 2]
-# label_cols = [3]
-
-# # Read training and validation data named tuples into memory.
-# train_tuple = load_labeled_csv(train_filename, feature_cols, label_cols)
-# validation_tuple = load_labeled_csv(validation_filename, feature_cols, label_cols)
-
-# random_seed = 42
-# nb_epochs = 50
-
-# # Search space
-# space = [(1,14), 
-#          (1,14),
-#          (1,14),
-#          (-7.0, -1),
-#          (3,11),
-#          (0.25,1.0)
-#         ]
-
-## HESTON
-## ===========================================================================
+# IMPLIED VOLATILITY
+# ===========================================================================
 
 # Data directories & data extraction params
-train_filename = 'deep_heston/labeled_data/train_uniform.csv'
-validation_filename = 'deep_heston/labeled_data/validation_uniform.csv'
-test_filename = 'deep_heston/labeled_data/test_uniform.csv'
-feature_cols = [i for i in range(7)]
-label_cols = [7]
+train_filename = 'deep_impliedvol/labeled_data/train_uniform.csv'
+validation_filename = 'deep_impliedvol/labeled_data/validation_uniform.csv'
+test_filename = 'deep_impliedvol/labeled_data/test_uniform.csv'
+feature_cols = [0, 1, 2]
+label_cols = [3]
 
-random_seed = 100
+# Read training and validation data named tuples into memory.
+train_tuple = load_labeled_csv(train_filename, feature_cols, label_cols)
+validation_tuple = load_labeled_csv(validation_filename, feature_cols, label_cols)
+
+random_seed = 42
 nb_epochs = 15
 
 # Search space
 space = [(1,10), 
          (1,10),
          (1,10),
-         (-7.0, -1.0),
+         (-7.0, -1),
          (3,10),
-         (0.25,1.0)
         ]
 
-# Read training and validation data named tuples into memory.
-train_tuple = load_labeled_csv(train_filename, feature_cols, label_cols)
-validation_tuple = load_labeled_csv(validation_filename, feature_cols, label_cols)
+## HESTON
+## ===========================================================================
+
+# # Data directories & data extraction params
+# train_filename = 'deep_heston/labeled_data/train_uniform.csv'
+# validation_filename = 'deep_heston/labeled_data/validation_uniform.csv'
+# test_filename = 'deep_heston/labeled_data/test_uniform.csv'
+# feature_cols = [i for i in range(7)]
+# label_cols = [7]
+
+# random_seed = 100
+# nb_epochs = 15
+
+# # Search space
+# space = [(1,10), 
+#          (1,10),
+#          (1,10),
+#          (-7.0, -1.0),
+#          (3,10),
+#          (0.25,1.0)
+#         ]
+
+# # Read training and validation data named tuples into memory.
+# train_tuple = load_labeled_csv(train_filename, feature_cols, label_cols)
+# validation_tuple = load_labeled_csv(validation_filename, feature_cols, label_cols)
 
 ## OPTIMIZATION
 ## ===========================================================================
 
 res_gp = gp_minimize(train_bayes, space, n_calls=200, n_random_starts=20, acq_func='EIps',
-                     n_jobs=-1, verbose=True, random_state=random_seed)
+                     n_jobs=-1, verbose=True, random_state=random_seed+1)
 
 dump(res_gp, filename + '.gz')
 
