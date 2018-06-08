22cca740b87477d5d7d796b9175d9fb567c68e2b
Benjamin Stemper <ben.m.stemper@gmail.com>
2018-06-08 15:59:59 +0200

    Include hyperdash at main train single level.

--- code/main_train_single_nn.py
+++ code/main_train_single_nn.py
@@ -3,12 +3,13 @@
 import numpy as np
 import tensorflow as tf
 import pandas as pd
-import logging
 import sys
 from ann.helpers import *
-from ann.train import train
-
+from ann.train import *
+from hyperdash import Experiment
 import os
+import logging
+import logging.config
 from os.path import dirname as up
 
 deep_cal_dir = up(os.getcwd())
@@ -16,27 +17,27 @@ deep_cal_dir = up(os.getcwd())
 # --------------------- Logging stuff ------------------------------------- #
 
 # Logging stuff
-logger = logging.getLogger("main")
+logger = logging.getLogger('deep_cal')
 logger.setLevel(logging.INFO)
 fh = logging.FileHandler("logs/single_training_nn.log")    
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 logger.addHandler(fh)
 
+
 # --------------------- Configuration ------------------------------------- #
 
 # Labeled data configuration
-train_filename = deep_cal_dir + '/data/heston/training_data.csv'
-validation_filename = deep_cal_dir + '/data/heston/validation_data.csv'
-feature_cols = [ _ for _ in range(7)]
-label_cols = [7]
-
+train_filename = deep_cal_dir + '/data/rough_bergomi/training_data.csv'
+validation_filename = deep_cal_dir + '/data/rough_bergomi/validation_data.csv'
+feature_cols = [ _ for _ in range(6)]
+label_cols = [6]
 # Model and training configuration
-random_seed = 1
-nb_epochs = 200
-mini_batch_size = 64
-layer_sizes = [256]*3
-lr = 1E-5
+random_seed = 10
+nb_epochs = 300
+mini_batch_size = 2048
+layer_sizes = [64]*10
+lr = 1E-3
 pkeep = 1
 
 hyper_params = [layer_sizes, lr, mini_batch_size, pkeep]
@@ -68,12 +69,17 @@ logger.info('Finished importing and normalization of input data.')
 
 # ------------------------ Training --------------------------------------- #
 
+hd_exp = Experiment(hyper_param_str)
+
 # Run backpropagation training. 
 df, best_error = train(train_tuple, validation_tuple, hyper_params, nb_epochs, 
-                       random_seed, verbose=True)
+                       random_seed, hd_exp, deep_cal_dir + '/code/')
 
 logger.info('Writing log dataframe to csv on disk.')
 df.to_csv(hyper_param_str + '/log_file.csv')
+
+# Finish Hyperdash experiment.
+hd_exp.end()
 
 logger.info("PROGRAM END.")
 
